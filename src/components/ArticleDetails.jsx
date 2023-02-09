1ed8c2c635af444c4eff32fe71545906d19e3ab2
abigail-a-lee <aabigaillee@gmail.com>
2023-02-09 16:49:20 +0000

    added comment input box

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getArticleById, getCommentsById } from "../utils/api";
 import Voting from "./common/Voting";
 import moment from "moment";
+import CommentInput from "./CommentInput";
 import Comments from "./Comments";
 import LoadingScreen from "./common/LoadingScreen";
 import { useParams } from "react-router-dom";
@@ -20,34 +21,31 @@ function ArticleDetails({ handleTitleClick }) {
   const { articleId } = useParams();
 
   useEffect(() => {
-    Promise.all([
-    getArticleById(articleId),
-    getCommentsById(articleId),
-  ])
-    .then(([articleResponse, commentsResponse]) => {
-      setArticleData(articleResponse.data.article[0]);
-      setCommentData(commentsResponse.data.comments);
-      setLoading(false);
-    });
+    Promise.all([getArticleById(articleId), getCommentsById(articleId)]).then(
+      ([articleResponse, commentsResponse]) => {
+        setArticleData(articleResponse.data.article[0]);
+        setCommentData(commentsResponse.data.comments);
+        setLoading(false);
+      }
+    );
   }, [articleId]);
 
   if (loading) {
     return <LoadingScreen />;
   }
 
-  
   return (
-    <main className="max-w-3xl mx-auto">
-      <section className="flex flex-row drop-shadow-md mx-auto pt-3 pb-6 dark:bg-neutral-900 text-base rounded-md">
-        <aside className="pr-10 text-neutral-400 place-content-center text-center">
-              <div className="absolute">
-              <Voting id={articleData.article_id} votes={articleData.votes} />
-              </div>
-            </aside>
-            <article className="flex-col flex"> 
+    <main className="text-md md:text-base max-w-[95vw]">
+      <section className="flex flex-row drop-shadow-md pt-3 pb-6 dark:bg-neutral-900 rounded-md">
+        <aside className="flex-col mx-1 min-w-[40px] text-neutral-400 place-content-center text-center">
+          <div className="absolute">
+            <Voting id={articleData.article_id} votes={articleData.votes} />
+          </div>
+        </aside>
+        <article className="flex-col flex max-w-[80%]">
           <header className="flex-row flex items-center rounded-md pb-1">
             {articleData.topic && (
-              <p className="inline-flex text-sm mr-1 text-neutral-900 dark:text-neutral-400">
+              <p className="inline-flex text:xs md:text-sm mr-1 text-neutral-900 dark:text-neutral-400">
                 {}
                 <span
                   className="hoverable mr-1"
@@ -76,36 +74,37 @@ function ArticleDetails({ handleTitleClick }) {
                 </time>
               </p>
             )}
-                                  
-
           </header>
 
           <h3 className=" pb-1 text-xl  font-bold text-neutral-900 dark:text-neutral-400">
             {articleData.title && <span>{articleData.title}</span>}
           </h3>
           {articleData.article_img_url && (
-            <img className="my-4 ring-2 ring-neutral-600 rounded-lg drop-shadow-md max-w-[700px]" src={articleData.article_img_url} alt=''></img>
+            <img
+              className="my-4 ring-1 ring-neutral-800 rounded-lg drop-shadow-md max-w-[85vw]"
+              src={articleData.article_img_url}
+              alt=""
+            ></img>
           )}
           {articleData.body && (
-            <p className=" text-neutral-500 dark:text-neutral-500 text-md mr-6">
+            <p className="text-neutral-500 dark:text-neutral-500 md:text-md">
               {articleData.body}
             </p>
           )}
-          </article>
-
+        </article>
       </section>
       <section className="bg-neutral-900 p-1 my-4 rounded-md">
-      <hr className="mt-10 solid max-w-8xl mx-12"></hr>
-            <div className="flex justify-between items-center mb-6">
-              <h2 className="text-lg pt-4 lg:text-2xl mx-12 font-bold text-neutral-900 dark:text-neutral-600">
-                Comments
-              </h2>
-            </div>
-            
-              <Comments commentData={commentData} />
-            </section>
-      </main>
+        <hr className="mt-10 solid max-w-8xl mx-12"></hr>
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-lg pt-4 lg:text-2xl mx-12 font-bold text-neutral-900 dark:text-neutral-600">
+            Comments
+          </h2>
+        </div>
+        <CommentInput />
+        <Comments commentData={commentData} />
+      </section>
+    </main>
   );
 }
 
-export default ArticleDetails
+export default ArticleDetails;
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,122 +1,124 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import moment from "moment";
 import avatar from "./common/avatar.jpeg";
 
 function date(value) {
-    return moment(value).format("ddd, MMM Do, YYYY h:mm:ss A");
-  }
-  function fromNow(value) {
-    return moment(value).fromNow();
-  }
+  return moment(value).format("ddd, MMM Do, YYYY h:mm:ss A");
+}
+function fromNow(value) {
+  return moment(value).fromNow();
+}
 
-function Comments({commentData}) {
-    const [isOpen, setOpen] = useState(false);
+function Comments({ commentData }) {
+  const [isOpen, setOpen] = useState(false);
 
-    const handleDropDown = () => {
-        setOpen(!isOpen);
-      };
-
+  const handleDropDown = () => {
+    setOpen(!isOpen);
+  };
 
   return (
     <div>
-       {commentData.map((comment, index) => (
-            <article key={index} className="px-2 mb-8 text-base bg-white  rounded-lg dark:bg-opacity-[0%]">
-              <img
-                className="mr-2.5 w-8 h-8 float-left rounded-full"
-                src={avatar}
-                alt="avatar"
-              />
-              <footer className="flex justify-between items-center mb-2">
-                <div className="flex items-center">
-                  {comment.author && (
-                    <p className="inline-flex items-center font-bold mr-2 text-sm text-neutral-900 dark:text-neutral-400">
-                      {comment.author}
-                    </p>
-                  )}
-                  <p className="inline-flex items-center mr-2 text-xs text-neutral-900 dark:text-white">
-                    •
-                  </p>
-                  {comment.created_at && (
-                    <p className="text-sm text-neutral-600 dark:text-neutral-600">
-                      <time
-                        dateTime={comment.created_at}
-                        title={date(comment.created_at)}
-                      >
-                        {fromNow(comment.created_at)}
-                      </time>
-                    </p>
-                  )}
-                </div>
+      {commentData.map((comment, index) => (
+        <article
+          key={index}
+          className="px-2 mb-8 text-base bg-white  rounded-lg dark:bg-opacity-[0%]"
+        >
+          <img
+            className="mr-2.5 w-8 h-8 float-left rounded-full"
+            src={avatar}
+            alt="avatar"
+          />
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              {comment.author && (
+                <p className="inline-flex items-center font-bold mr-2 text-sm text-neutral-900 dark:text-neutral-400">
+                  {comment.author}
+                </p>
+              )}
+              <p className="inline-flex items-center mr-2 text-xs text-neutral-900 dark:text-white">
+                •
+              </p>
+              {comment.created_at && (
+                <p className="text-sm text-neutral-600 dark:text-neutral-600">
+                  <time
+                    dateTime={comment.created_at}
+                    title={date(comment.created_at)}
+                  >
+                    {fromNow(comment.created_at)}
+                  </time>
+                </p>
+              )}
+            </div>
 
-                <button
-                  id="dropdownCommentButton"
-                  data-dropdown-toggle=""
-                  className="inline-flex items-center p-2 mr-8 font-medium text-center text-neutral-400 bg-white rounded-lg hover:bg-neutral-100 focus:ring-2 focus:outline-none focus:ring-neutral-500 dark:bg-neutral-900 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600"
-                  type="button"
-                  onClick={handleDropDown}
-                >
-                  <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                  </svg>
-                  <span className="sr-only">Comment settings</span>
-                </button>
+            <button
+              id="dropdownCommentButton"
+              data-dropdown-toggle=""
+              className="inline-flex items-center p-2 mr-8 font-medium text-center text-neutral-400 bg-white rounded-lg hover:bg-neutral-100 focus:ring-2 focus:outline-none focus:ring-neutral-500 dark:bg-neutral-900 dark:hover:bg-neutral-700 dark:focus:ring-neutral-600"
+              type="button"
+              onClick={handleDropDown}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+              </svg>
+              <span className="sr-only">Comment settings</span>
+            </button>
 
-                <div
-                  id="dropdownComment1"
-                  className={`${
-                    isOpen ? "block" : "hidden"
-                  } z-10 w-36 bg-white absolute right-0 rounded divide-y divide-neutral-100 shadow dark:bg-neutral-700 dark:divide-neutral-600`}
+            <div
+              id="dropdownComment1"
+              className={`${
+                isOpen ? "block" : "hidden"
+              } z-10 w-36 bg-white absolute right-0 rounded divide-y divide-neutral-100 shadow dark:bg-neutral-700 dark:divide-neutral-600`}
+            >
+              <ul
+                className="py-1 text-sm text-neutral-700 dark:text-neutral-200"
+                aria-labelledby="dropdownMenuIconHorizontalButton"
+              >
+                <li>Edit</li>
+                <li>Remove</li>
+                <li>Report</li>
+              </ul>
+            </div>
+          </footer>
+          <div className="mx-4 border-neutral-700 px-6 border-solid border-l-[2px]">
+            {comment.body && (
+              <p className="text-neutral-500 my-4 dark:text-neutral-500">
+                {comment.body}
+              </p>
+            )}
+            <div className="flex items-center space-x-4">
+              <button
+                type="button"
+                className="flex items-center  text-sm text-neutral-500 hover:underline dark:text-neutral-400"
+              >
+                <svg
+                  aria-hidden="true"
+                  className="mr-1 w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <ul
-                    className="py-1 text-sm text-neutral-700 dark:text-neutral-200"
-                    aria-labelledby="dropdownMenuIconHorizontalButton"
-                  >
-                    <li>Edit</li>
-                    <li>Remove</li>
-                    <li>Report</li>
-                  </ul>
-                </div>
-              </footer>
-              <div className="mx-4 border-neutral-700 px-6 border-solid border-l-[2px]">
-                {comment.body && (
-                  <p className="text-neutral-500 my-4 dark:text-neutral-500">
-                    {comment.body}
-                  </p>
-                )}
-                <div className="flex items-center space-x-4">
-                  <button
-                    type="button"
-                    className="flex items-center  text-sm text-neutral-500 hover:underline dark:text-neutral-400"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="mr-1 w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                      ></path>
-                    </svg>
-                    Reply
-                  </button>
-                </div>
-              </div>
-            </article>
-          ))}
-        </div>
-  )
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                  ></path>
+                </svg>
+                Reply
+              </button>
+            </div>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 }
 
-export default Comments
+export default Comments;
